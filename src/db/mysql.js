5d4751db61bdfbc9556cc3074ab64f346e056423
杨有义 <yang_you_yi@163.com>
2021-11-01 00:24:41 +0800

    remove commented code

--- src/db/mysql.js
+++ src/db/mysql.js
@@ -4,15 +4,5 @@ const sequelize = new Sequelize('parking_lot', 'root', 'root', {
     port: '3306',
     dialect: 'mysql'
 })
-
-// async function f1 () {
-//     try {
-//         await sequelize.authenticate()
-//         console.log('Connection has been established successfully.')
-//     } catch (error) {
-//         console.error('Unable to connect to the database:', error)
-//     }
-// }
-// f1()
     
 module.exports = sequelize
