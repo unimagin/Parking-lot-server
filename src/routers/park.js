379757d1b29eb795c9d65c31b54a51421b985f40
杨有义 <yang_you_yi@163.com>
2021-11-03 11:45:43 +0800

    add router for showing status

--- src/routers/park.js
+++ src/routers/park.js
@@ -5,7 +5,7 @@ router.post('/user/reservation', async (request, response) => {
     try {
         const base = request.body.floor_level * 50
         let array = new Array(50)
-        for (let i = 0; i < 50; ++i) {
+        for (let i = 1; i <= 50; ++i) {
             array[i] = {parking_number: base + i, status: i%3+1}
         }
         response.send(array)
