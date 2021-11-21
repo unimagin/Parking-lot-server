083c7d1ae0756081c70f5aa046607ab181c118a4
0whatever <840065505@qq.com>
2021-11-21 16:04:55 +0800

    fix time_format

--- src/routers/bill.js
+++ src/routers/bill.js
@@ -3,23 +3,18 @@ const Bill = require('../models/bill')
 const Reservation = require('../models/reservation')
 const router = new express.Router()
 
-function dateTranse (mydate) {
-  const d_arr = mydate.split('T')[0].split('-')
-  const t = mydate.split('T')[1].split('.')[0]
-  const str = d_arr[1] + " " + d_arr[2] + "," + d_arr[0] + " " + t
-  const r_date = new Date(str)
-  return r_date
-}
-
 router.post('/bill/generate_bill', async (request, response) => {
   const reservation = request.body
   try {
-    const begin_time = dateTranse(reservation.begin_time)
-    const end_time = dateTranse(reservation.end_time)
-    const leave_time = new Date()
-    const date = reservation.begin_time.split('T')[0]
-    const arrive_time = dateTranse(date + "T" + reservation.arrive_time + ".")
-    let status = -1, money = 0
+    const begin = new Date(reservation.begin_time)
+    const begin_time = begin.getTime()
+    const end = new Date(reservation.end_time)
+    const end_time = end.getTime()
+    const leave = new Date()
+    const leave_time = leave.getTime()
+    const arrive = new Date(reservation.arrive_time)
+    const arrive_time = arrive.getTime()
+    let status = 0, money = 0
     if (arrive_time > begin_time) {
       status = 1
     }
@@ -35,8 +30,8 @@ router.post('/bill/generate_bill', async (request, response) => {
         user_ID: reservation.user_ID,
         arrive_time: arrive_time,
         leave_time: leave_time,
-        begin_time: reservation.begin_time,
-        end_time: reservation.end_time,
+        begin_time: begin_time,
+        end_time: end_time,
         money: 1,
         status: status,
         isPaid: 0
--- src/routers/reservation.js
+++ src/routers/reservation.js
@@ -4,9 +4,8 @@ const authentication = require('../middleware/auth')
 const router = new express.Router()
 
 router.post('/reservation/change_used', async (request, response) => {
-  const { row, now } = request.body
-  const reservation_ID = row.reservation_ID
-  const arrive_time = now
+  const reservation_ID = request.body.reservation_ID
+  const arrive_time = new Date(request.body.arrive_time)
   try {
     const reservation = await Reservation.findOne({ where: { reservation_ID: reservation_ID } })
     if (reservation == null) {
@@ -25,44 +24,44 @@ router.post('/reservation/change_used', async (request, response) => {
 })
 
 router.post('/user/appoint', authentication, async (request, response) => {
-  let {car_number, parking_number, begin_time, end_time} = request.body
-  const {user_ID} = request.user
+  let { car_number, parking_number, begin_time, end_time } = request.body
+  const { user_ID } = request.user
   try {
-      begin_time = new Date(begin_time)
-      end_time = new Date(end_time)
-      begin_timestamp = Date.parse(begin_time)
-      end_timestamp = Date.parse(end_time)
-      let reservs = await Reservation.findAll({where: {parking_number}})
-      reservs = reservs.map((x) => {
-          x.begin_timestamp = Date.parse(x.begin_time)
-          x.end_timestamp = Date.parse(x.end_time)
-          return x
-      })
-      reservs = reservs.sort((x1, x2) => {
-          return x1.begin_timestamp - x2.begin_timestamp
-      })
-      
-      let left = 0, right = reservs.length - 1, mid, avaliable = true
-      while (left <= right) {
-        mid = Math.floor((left + right)/2)
-        if (reservs[mid].end_timestamp <= begin_timestamp) {
-          left = mid + 1
-        }
-        else if (reservs[mid].begin_timestamp >= end_timestamp) {
-          right = mid - 1
-        }
-        else {
-          avaliable = false
-          break
-        }
+    begin_time = new Date(begin_time)
+    end_time = new Date(end_time)
+    begin_timestamp = Date.parse(begin_time)
+    end_timestamp = Date.parse(end_time)
+    let reservs = await Reservation.findAll({ where: { parking_number } })
+    reservs = reservs.map((x) => {
+      x.begin_timestamp = Date.parse(x.begin_time)
+      x.end_timestamp = Date.parse(x.end_time)
+      return x
+    })
+    reservs = reservs.sort((x1, x2) => {
+      return x1.begin_timestamp - x2.begin_timestamp
+    })
+
+    let left = 0, right = reservs.length - 1, mid, avaliable = true
+    while (left <= right) {
+      mid = Math.floor((left + right) / 2)
+      if (reservs[mid].end_timestamp <= begin_timestamp) {
+        left = mid + 1
       }
-      if (!avaliable) {
-        throw new Error('Appointment failed. Time is not availble')
+      else if (reservs[mid].begin_timestamp >= end_timestamp) {
+        right = mid - 1
       }
-      await Reservation.create({user_ID, car_number, parking_number, begin_time, end_time})
-      response.send({state:true})
-  } catch(error) {
-      response.status(400).send({state: false})
+      else {
+        avaliable = false
+        break
+      }
+    }
+    if (!avaliable) {
+      throw new Error('Appointment failed. Time is not availble')
+    }
+    await Reservation.create({ user_ID, car_number, parking_number, begin_time, end_time })
+    response.send({ state: true })
+  } catch (error) {
+    response.status(400).send({ state: false })
   }
 })
 
