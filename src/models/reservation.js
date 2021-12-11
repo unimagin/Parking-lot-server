b050f8738c0de3d4a7e0789c3dfd0d7e5c5d6c1a
杨有义 <yang_you_yi@163.com>
2021-12-11 20:57:43 +0800

    重构查询车位可用性代码

--- src/models/reservation.js
+++ src/models/reservation.js
@@ -1,9 +1,33 @@
-const { Sequelize, DataTypes } = require('sequelize')
+const { Sequelize, DataTypes, Model, Op } = require('sequelize')
 const sequelize = require('../db/mysql')
 const User = require('./user')
 const Car = require('./car')
 
-const Reservation = sequelize.define('Reservation', {
+class Reservation extends Model {
+    static async isAvailable(parking_number, begin_time, end_time, reservation_ID = undefined) {
+        begin_time = new Date(begin_time)
+        end_time = new Date(end_time)
+        begin_time = Date.parse(begin_time)
+        end_time = Date.parse(end_time)
+        let reservation = null
+        if (reservation_ID != undefined && (reservation = await Reservation.findByPk(reservation_ID)) == null) {
+            return false; //如果选择修改，但ID不存在，返回false
+        }
+        const reservs = reservation == null ? //如果是修改预约，则判断时间冲突前需要去掉自己
+            await Reservation.findAll({ where: { parking_number } }) :
+            await Reservation.findAll({ where: { parking_number, reservation_ID: { [Op.ne]: reservation_ID } } })
+        let available = true
+        for (let i = 0; i < reservs.length; ++i) {
+            if (!(reservs[i].end_time.getTime() <= begin_time || reservs[i].begin_time.getTime() >= end_time)) {
+                available = false
+                break
+            }
+        }
+        return available
+    }
+}
+
+Reservation.init({
     reservation_ID: {
         type: DataTypes.UUID,
         primaryKey: true,
@@ -34,6 +58,8 @@ const Reservation = sequelize.define('Reservation', {
         defaultValue: 0
     }
 }, {
+    sequelize,
+    modelName: 'Reservation'
 })
 
 Reservation.belongsTo(User, {
